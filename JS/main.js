564d76ec2f59344577d441601e39b36df322e3a4
Alejandro Molina <molina_castt@hotmail.com>
2023-06-26 21:13:04 -0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+// let entrada = prompt("Ingrese un dato");
+
+// while (entrada != "ESC"){
+//     alert("El usuario ingreso " + entrada);
+//     entrada = prompt("Ingrese un dato");
+
+// }
+
+// let numero = 0;
+
+// do{
+//     numero = prompt("Ingresar Numero");
+//     console.log(numero);
+// }while(parseInt(numero));
+
+// CONTADOR DE OVEJAS CON WHILE
+
+// let i = 0;
+
+// while (i < 11){
+//     document.write("Tengo " + i + " oveja <br>")
+//     i += 1
+// }
+
